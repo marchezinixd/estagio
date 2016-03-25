e6dd8f6306cf6e0662fa2cc770b7e44a74f8b042
VANESSA MARCHEZINI <VANESSA MARCHEZINI>
2016-03-25 17:02:17 -0300

    Entrada completa

--- K-means.py
+++ K-means.py
@@ -4,7 +4,7 @@ import math
 import random
 import subprocess
 import numpy
-
+sample=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 def  Leitor(File):
     maxi=0;
     with open(File, 'r') as f:
@@ -12,6 +12,7 @@ def  Leitor(File):
         l=0
 
         pointreturn=[]
+
 
         for row in reader :
             if(l!=0 ):
@@ -32,14 +33,82 @@ def  Leitor(File):
                     if (certo ==-1):
                         certo=acerta(contador,point)
 
-                pointsin=Point(point )
+                pointsin=Point(point)
+                defsample(pointsin, row)
 
-                    #print pointsin
 
                 pointreturn.append(pointsin)
             l=l+1
 
     return pointreturn
+
+def defsample(point,row):
+    if (row[1]=="A001"):
+        global sample[0]=point
+    if (row[1]=="A002"):
+        global sample[1]=point
+    if (row[1]=="A003"):
+        global sample[2]=point
+    if (row[1]=="A004"):
+        global sample[3]=point
+    if (row[1]=="A005"):
+        global sample[4]=point
+    if (row[1]=="A006"):
+        global sample[5]=point
+    if (row[1]=="A007"):
+        global sample[6]=point
+    if (row[1]=="A008"):
+        global sample[7]=point
+    if (row[1]=="A009"):
+        global sample[8]=point
+    if (row[1]=="A010"):
+        global sample[9]=point
+    if (row[1]=="A011"):
+        global sample[10]=point
+    if (row[1]=="A012"):
+        global sample[11]=point
+    if (row[1]=="A013"):
+        global sample[12]=point
+    if (row[1]=="A014"):
+        global sample[13]=point
+    if (row[1]=="A015"):
+        global sample[14]=point
+    if (row[1]=="A016"):
+        global sample[15]=point
+    if (row[1]=="A017"):
+        global sample[16]=point
+    if (row[1]=="A018"):
+        global sample[17]=point
+    if (row[1]=="A019"):
+        global sample[18]=point
+    if (row[1]=="A020"):
+        global sample[19]=point
+    if (row[1]=="A021"):
+        global sample[20]=point
+    if (row[1]=="A022"):
+        global sample[21]=point
+    if (row[1]=="A023"):
+        global sample[22]=point
+    if (row[1]=="A024"):
+        global sample[23]=point
+    if (row[1]=="A025"):
+        global sample[24]=point
+    if (row[1]=="A026"):
+        global sample[25]=point
+    if (row[1]=="A027"):
+        global sample[26]=point
+    if (row[1]=="A028"):
+        global sample[27]=point
+    if (row[1]=="A029"):
+        global sample[28]=point
+    if (row[1]=="A030"):
+        global sample[29]=point
+    if (row[1]=="A031"):
+        global sample[30]=point
+    if (row[1]=="A032"):
+        global sample[31]=point
+    if (row[1]=="A033"):
+        global sample[32]=point
 
 def acerta(posicao,point):
     if (posicao==0):
@@ -49,46 +118,175 @@ def acerta(posicao,point):
                     point[0]=(point[2]+point[3]+point[6])/3
 
                 else:
-                    acerta(6,points)
+                    acerta(6,point)
             else:
-                acerta(3,points)
+                acerta(3,point)
         else:
-            acerta(2,points)
+            acerta(2,point)
     elif (posicao==1):
+	   if(point[3]!=-1):
+	       if(point[4]!=-1):
+			if(point[7]):
+				point[1]=(point[3]+point[4]+point[7])/3
+			else:
+				acerta(7,point)
+		else:
+			acerta(4,point)
+	else:
+		acerta(3,point)
 
-    '''elif (posicao==2):
+    elif (posicao==2):
+    	if(point[5]!=-1):
+    		if(point[6]!=-1):
+    			point[2]=(point[5]+point[6])/2
+    		else:
+    			acerta(6,point)
+    	else:
+    		acerta(5,point)
 
     elif (posicao==3):
+    	if(point[2]!=-1):
+    		if(point[4]!=-1):
+    			point[3]=(point[2]+point[4])/2
+    		else:
+    			acerta(4,point)
+    	else:
+    		acerta(2,point)
 
     elif (posicao==4):
+    	if(point[7]!=-1):
+    		if(point[8]!=-1):
+    			point[4]=(point[7]+point[8])/2
+    		else:
+    			acerta(8,point)
+    	else:
+    		acerta(7,point)
 
     elif (posicao==5):
+    	if(point[6]!=-1):
+    		if(point[11]!=-1):
+    			point[5]=(point[6]+point[11])/2
+    		else:
+    			acerta(11,point)
+    	else:
+    		acerta(6,point)
 
     elif (posicao==6):
+    	if(point[1]!=-1):
+    		if(point[11]!=-1):
+    			point[6]=(point[1]+point[11])/2
+    		else:
+    			acerta(11,point)
+    	else:
+    		acerta(1,point)
 
     elif (posicao==7):
+    	if(point[3]!=-1):
+    		if(point[10]!=-1):
+    			point[7]=(point[4]+point[10])/2
+    		else:
+    			acerta(10,point)
+    	else:
+    		acerta(3,point)
 
     elif (posicao==8):
+    	if(point[7]!=-1):
+    		if(point[14]!=-1):
+    			point[8]=(point[7]+point[14])/2
+    		else:
+    			acerta(14,point)
+    	else:
+    		acerta(7,point)
 
     elif (posicao==9):
+    	if(point[6]!=-1):
+    		if(point[12]!=-1):
+    			point[9]=(point[6]+point[12])/2
+    		else:
+    			acerta(12,point)
+    	else:
+    		acerta(6,point)
 
     elif (posicao==10):
+    	if(point[8]!=-1):
+    		if(point[13]!=-1):
+    			point[10]=(point[8]+point[13])/2
+    		else:
+    			acerta(13,point)
+    	else:
+    		acerta(8,point)
 
     elif (posicao==11):
+    	if(point[12]!=-1):
+    		if(point[17]!=-1):
+    			point[11]=(point[12]+point[17])/2
+    		else:
+    			acerta(12,point)
+    	else:
+    		acerta(17,point)
 
     elif (posicao==12):
+    	if(point[6]!=-1):
+    		if(point[15]!=-1):
+    			point[12]=(point[6]+point[15])/2
+    		else:
+    			acerta(6,point)
+    	else:
+    		acerta(15,point)
 
     elif (posicao==13):
+    	if(point[12]!=-1):
+    		if(point[14]!=-1):
+    			point[13]=(point[12]+point[14])/2
+    		else:
+    			acerta(14,point)
+    	else:
+    		acerta(12,point)
 
     elif (posicao==14):
+    	if(point[8]!=-1):
+    		if(point[18]!=-1):
+    			point[14]=(point[8]+point[18])/2
+    		else:
+    			acerta(18,point)
+    	else:
+    		acerta(8,point)
 
     elif (posicao==15):
+    	if(point[16]!=-1):
+    		if(point[17]!=-1):
+    			point[15]=(point[16]+point[17])/2
+    		else:
+    			acerta(17,point)
+    	else:
+    		acerta(16,point)
 
     elif (posicao==16):
+    	if(point[13]!=-1):
+    		if(point[18]!=-1):
+    			point[16]=(point[13]+point[18])/2
+    		else:
+    			acerta(18,point)
+    	else:
+    		acerta(13,point)
 
     elif (posicao==17):
+    	if(point[12]!=-1):
+    		if(point[15]!=-1):
+    			point[17]=(point[12]+point[15])/2
+    		else:
+    			acerta(15,point)
+    	else:
+    		acerta(12,point)
 
-    elif (posicao==18):'''
+    elif (posicao==18):
+    	if(point[13]!=-1):
+    		if(point[16]!=-1):
+    			point[18]=(point[13]+point[16])/2
+    		else:
+    			acerta(16,point)
+    	else:
+    		acerta(13,point)
     return
 
 def main():
@@ -183,8 +381,8 @@ class Cluster:
 def kmeans(points, k, cutoff):
 
     # Pick out k random points to use as our initial centroids
-    initial = random.sample(points, k)
-
+    #initial = random.sample(points, k)
+    initial = global sample
     # Create k clusters using those centroids
     clusters = [Cluster([p]) for p in initial]
 
